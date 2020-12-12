7240adf4349c44d666c87fc859ba1f5c09e626e9
jarekTerejko <j.terejko@gmail.com>
2020-12-12 06:18:44 +0100

    create addTodo function

--- todo-app-main/src/Components/TextInput/index.js
+++ todo-app-main/src/Components/TextInput/index.js
@@ -1,12 +1,32 @@
-import React from "react";
-import {TextInputWrapper, TextInputCircle, TextInputEl} from './TextInputElements'
+import React, { useContext, useState } from "react";
+import {
+  TextInputWrapper,
+  TextInputCircle,
+  TextInputEl,
+} from "./TextInputElements";
+import { TodosContext } from "../../contexts/TodosContext";
 
 const TextInput = () => {
+  const { addTodo } = useContext(TodosContext);
+
+  const [todoText, setTodoText] = useState("");
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    addTodo(todoText);
+    setTodoText("");
+  };
+
   return (
-      
-    <TextInputWrapper>
+    <TextInputWrapper onSubmit={handleOnSubmit}>
       <TextInputCircle></TextInputCircle>
-      <TextInputEl type="text" name="todo-input" id="todo-input" />
+      <TextInputEl
+        type="text"
+        value={todoText}
+        name="todo-input"
+        id="todo-input"
+        onChange={(e) => setTodoText(e.target.value)}
+      />
     </TextInputWrapper>
   );
 };
--- todo-app-main/src/contexts/TodosContext.js
+++ todo-app-main/src/contexts/TodosContext.js
@@ -4,9 +4,21 @@ export const TodosContext = createContext();
 
 const TodosContextProvider = (props) => {
   const [todos, setTodos] = useState([]);
+  const [todosToRender, setTodosToRender] = useState(todos);
+
+  const addTodo = (todoText) => {
+    setTodos([
+      ...todos,
+      {
+        isCompleted: false,
+        todoText: todoText.trim(),
+        id: new Date().getTime(),
+      },
+    ]);
+  };
 
   return (
-    <TodosContext.Provider value={{ todos, setTodos }}>
+    <TodosContext.Provider value={{ todos, setTodos, addTodo, todosToRender }}>
       {props.children}
     </TodosContext.Provider>
   );
