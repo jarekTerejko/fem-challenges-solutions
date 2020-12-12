81a5013e612f22b972234117d860c6b1c574aad9
jarekTerejko <j.terejko@gmail.com>
2020-12-12 04:49:14 +0100

    import components

--- todo-app-main/src/App.js
+++ todo-app-main/src/App.js
@@ -1,9 +1,25 @@
-function App() {
+import React from "react";
+import { Container } from "./Components/Container";
+import Header from "./Components/Header";
+import TextInput from "./Components/TextInput";
+import Todo from "./Components/Todo";
+import TodosContextProvider from "./contexts/TodosContext";
+
+const App = () => {
   return (
-    <div>
-      Todo App
-    </div>
+    <TodosContextProvider>
+      <div>
+        <Header />
+        <Container>
+          <TextInput />
+          <div>
+            <Todo />
+            <Todo />
+          </div>
+        </Container>
+      </div>
+    </TodosContextProvider>
   );
-}
+};
 
 export default App;
