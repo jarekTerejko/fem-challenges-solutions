f2c318d5592bd9a1c1096b2bac5c9ff54d74e7a4
jarekTerejko <j.terejko@gmail.com>
2020-12-22 04:01:33 +0100

    fix border radius

--- todo-app-main/src/Components/TodoList/TodoListElements.js
+++ todo-app-main/src/Components/TodoList/TodoListElements.js
@@ -1,3 +1,9 @@
 import styled from "styled-components";
 
-export const TodoListWrapper = styled.ul``;
+export const TodoListWrapper = styled.ul`
+  li:first-child {
+    border-top-left-radius: 0.5rem;
+    border-top-right-radius: 0.5rem;
+    background-color: var(--white);
+  }
+`;
