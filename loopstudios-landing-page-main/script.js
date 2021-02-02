c275bd23b582c41c5f95d2230e8ed774808baf87
jarekTerejko <j.terejko@gmail.com>
2021-02-02 08:05:03 +0100

    add gsap animations

--- loopstudios-landing-page-main/script.js
+++ loopstudios-landing-page-main/script.js
@@ -17,7 +17,7 @@ const cards = cardsData
     return `<a href="#" class="our-creations__card our-creations__${
       card.className
     }${i + 1}">
-            <span class="our-creations__card-title" >${card.title}</span>
+            <span class="our-creations__card-title">${card.title}</span>
         </a>`;
   })
   .join("");
@@ -51,3 +51,74 @@ window.addEventListener("scroll", () => {
   const navbar = document.querySelector(".navbar");
   navbar.classList.toggle("navbar__scrolled", window.scrollY > 0);
 });
+
+// gsap animations
+gsap.registerPlugin(ScrollTrigger);
+
+const animElements = document.querySelectorAll(".scroll-animation-from-bottom");
+
+animElements.forEach((element) => {
+  gsap.fromTo(
+    element.children,
+    { y: "+=120", opacity: 0 },
+    {
+      y: 0,
+      opacity: 1,
+      stagger: 0.3,
+      duration: 1,
+      ease: "easeInOut",
+      scrollTrigger: {
+        trigger: element,
+        start: "top 70%",
+        // toggleActions: "play pause resume reset",
+      },
+    }
+  );
+});
+
+const aboutWrapper = document.querySelector(".about__wrapper");
+
+const leftEl = document.querySelector(".about__bg-img");
+const rightEl = document.querySelector(".about__heading-container");
+
+gsap.fromTo(
+  leftEl,
+  { x: "-600", opacity: 0 },
+  {
+    x: 0,
+    opacity: 1,
+    duration: 1,
+    delay: 0.5,
+    ease: "easeInOut",
+    scrollTrigger: {
+      trigger: aboutWrapper,
+      start: "top 90%",
+      // toggleActions: "play pause resume reset",
+    },
+  }
+);
+
+gsap.fromTo(
+  rightEl,
+  { x: "-600", opacity: 0 },
+  {
+    x: 0,
+    opacity: 1,
+    duration: 1,
+    delay: 0.8,
+    ease: "bounce.out",
+    scrollTrigger: {
+      trigger: aboutWrapper,
+      start: "top 90%",
+      // toggleActions: "play pause resume reset",
+    },
+  }
+);
+
+gsap.fromTo(
+  ".header__heading",
+  { opacity: 0, x: 600 },
+  { opacity: 1, x: 0, ease: "power1.out", delay: 0.8, duration: 1 }
+);
+
+gsap.fromTo(".navbar", { opacity: 0 }, { opacity: 1, delay: 1, duration: 0.3 });
